8588338c2de2a0c39e6acb0360f1ec993dbd80f9
hexs <154236651+hexs@users.noreply.github.com>
2024-05-28 15:32:04 +0700

    Add files via upload

--- m2.py
+++ m2.py
@@ -1,21 +1,24 @@
+import itertools
 from itertools import permutations
 from pprint import pprint
 import requests
 
 list_characters = 'abcdefghijklmnopqrstuvwxyz'
-number_of_characters = 1
+number_of_characters = 3
 
-switch = [''.join(p) for p in permutations(list_characters, number_of_characters)]
-pprint(switch)  # ['ab', 'ac', 'ba', 'bc', 'ca', 'cb']
-
-p = switch
+# switch = [''.join(p) for p in permutations(list_characters, number_of_characters)]
+switch = [''.join(combo) for combo in itertools.product(list_characters, repeat=number_of_characters)]
 
 proxies = {
     "http": f"http://150.61.8.70:10080",
     "https": f"http://150.61.8.70:10080",
 }
 
-for name in p:
+for name in switch:
     response = requests.get(f'https://pypi.org/project/{name}/', proxies=proxies)
     if response.status_code != 200:
         print(f"{name} - {response.status_code}")
+        with open('name.txt', 'a') as f:
+            f.write(f'{name} - {response.status_code}\n')
+    else:
+        print(f"{name}")
